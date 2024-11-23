140c319695a488351c9fc717ffe5209c1e09e3ed
jwdev88 <johanw018@gmail.com>
2024-11-23 20:49:51 +0700

    done -about.contant

--- E-Commerce/src/context/ShopContext.jsx
+++ E-Commerce/src/context/ShopContext.jsx
@@ -1,59 +1,102 @@
-import React, { createContext, useEffect, useState } from 'react'; 
+import React, { createContext, useEffect, useState } from 'react';
 import { products } from '../assets/assets';
 
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
     const currency = 'Rp.';
-    const delivery_fee = 1000;
-    const [search,setSearch] = useState ('');
-    const [showSearch, setShowSearch] = useState(false) 
+    const delivery_fee = 100;
+    const [search, setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false)
+    const navigate = useNavigate();
 
     const [cartItems, setCartItems] = useState({});
-    const addToCart = async (itemId,size) =>{
-        
-        if(!size){
+    const addToCart = async (itemId, size) => {
+
+        if (!size) {
             toast.error('Select Product First')
         }
         let cartData = structuredClone(cartItems)
 
-        if (cartData[itemId] ){
-            if (cartData [itemId] [size]){
-                cartData [itemId] [size] +=1;
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
             }
-            else{
-                cartData [itemId] [size] = 1;
+            else {
+                cartData[itemId][size] = 1;
 
-            }   
+            }
         }
-        else{
+        else {
             cartData[itemId] = {};
-            cartData [itemId] [size] = 1;
+            cartData[itemId][size] = 1;
         }
         setCartItems(cartData)
     }
 
-    useEffect(()=>{
-    console.log(cartItems)
 
-    },[cartItems])
+    const getCartCount = () => {
+
+        let totalCount = 0;
+
+        for (const items in cartItems) {
+            for (const item in cartItems[items]) {
+
+                try {
+
+                    if (cartItems[items][item] > 0) {
+                        totalCount += cartItems[items][item];
+
+                    }
+
+                } catch (error) {
+                }
+
+            }
+
+        }
+        return totalCount;
+    }
 
 
+const getCartAmount = () =>{
+    let totalAmount = 0;
+    for(const items in cartItems){
+        let itemInfo = products.find((product)=>product._id === items);
+        for(const item in cartItems[items]){
+            try {
+                if(cartItems[items][item]>0){
+                    totalAmount +=itemInfo.price * cartItems[items][item];
+                }
+            } catch (error) {
+                
+            }
+        }
+    }
+    return totalAmount;
+}
 
-    const value = {
-        products, currency, delivery_fee,
-        search,setSearch,showSearch,setShowSearch,
-        cartItems,addToCart    
+const updateQuanity = async (itemId,size,quantity)=>{
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData)
+}
 
-    };
+const value = {
+    products, currency, delivery_fee,
+    search, setSearch, showSearch, setShowSearch,
+    cartItems, addToCart, getCartCount,updateQuanity,getCartAmount,navigate
 
-    return (
-        <ShopContext.Provider value={value}> 
-            {props.children}
-        </ShopContext.Provider>
-    );
+};
+
+return (
+    <ShopContext.Provider value={value}>
+        {props.children}
+    </ShopContext.Provider>
+);
 }
 
 export default ShopContextProvider;
--- E-Commerce/src/pages/Login.jsx
+++ E-Commerce/src/pages/Login.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  
+  const [currentState,setCurrentState] = useState('Login')
+  const onSubmitHandler = async (event)=>{
+    event.preventDefault();
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p className='prate-regular text-3xl'>{currentState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
+      </div>
+      {currentState === 'Login' ? '':<input className='w-full px-3 py-2 border border-gray-800' placeholder='name' type="text"  required/>}
+      <input className='w-full px-3 py-2 border border-gray-800' placeholder='email' type="email"  required/>
+      <input className='w-full px-3 py-2 border border-gray-800' placeholder='password' type="password" required />
+      <div className='w-full flex justify-between text-sm mt-[-8x]'>
+        <p className='cursor-pointer'>Forget your password?</p>
+        {
+          currentState ==='Login'
+          ? <p onClick={()=>setCurrentState('Sign up')} className='cursor-pointer'>Create account</p>
+          : <p onClick={()=>setCurrentState('Login')} className='cursor-pointer'>Login Here</p>
+        }
+      </div>
+        <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState==='Login' ? 'Sign in': 'Sign up'}</button>
+    </form>
   )
 }
+
 
 export default Login
